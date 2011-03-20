b4d7e3f7b4461b646146d2362b4c2bd99a96efa2
deadprogrammer <ron.evans@gmail.com>
2011-03-19 18:59:47 -0700

    Get compatible with Ruby 1.8.7 too

--- app/models/runner.rb
+++ app/models/runner.rb
@@ -1,35 +1,23 @@
 require 'htmlentities'
 
 class Runner < Qt::Process
+  slots 'readData()', 'readError()'
+  
   def initialize(main)
     super
     
     @main_widget = main
     @coder = HTMLEntities.new
     
-    connect(SIGNAL(:readyReadStandardOutput), &method(:read_data))
-    connect(SIGNAL(:readyReadStandardError),  &method(:read_error))
+    Qt::Object.connect(self, SIGNAL(:readyReadStandardOutput), 
+                          self, SLOT(:readData))
+    Qt::Object.connect(self, SIGNAL(:readyReadStandardError), 
+                          self, SLOT(:readError))                          
   end
 
   def run(code = default_code, code_file_name = default_kid_code_location)
     save_kid_code(code, code_file_name)
     self.start("ruby #{code_file_name}")
-  end
-
-  def read_data
-    out = self.readAllStandardOutput.data
-    out.split("\n").each do |line|
-      code = "updateStdOut('#{@coder.encode(line)}<br/>')"
-      @main_widget.evaluateJavaScript(code)
-    end
-  end
-  
-  def read_error
-    err = self.readAllStandardError.data
-    err.split("\n").each do |line|
-      code = "updateStdErr('#{@coder.encode(line)}<br/>')"
-      @main_widget.evaluateJavaScript(code)
-    end
   end
 
   def save_kid_code(code, code_file_name)
@@ -63,10 +51,28 @@ class Runner < Qt::Process
   end
   
   def ensure_tmp_dir
-    Dir.mkdir(tmp_dir) unless Dir.exists?(tmp_dir)
+    Dir.mkdir(tmp_dir) unless File.exists?(tmp_dir)
   end
   
   def tmp_dir
     File.expand_path(File.dirname(__FILE__) + "/../../tmp")
   end
+  
+  private
+
+  def readData
+    out = self.readAllStandardOutput.data
+    out.split("\n").each do |line|
+      code = "updateStdOut('#{@coder.encode(line)}<br/>')"
+      @main_widget.evaluateJavaScript(code)
+    end
+  end
+  
+  def readError
+    err = self.readAllStandardError.data
+    err.split("\n").each do |line|
+      code = "updateStdErr('#{@coder.encode(line)}<br/>')"
+      @main_widget.evaluateJavaScript(code)
+    end
+  end  
 end
--- lib/kidsruby.rb
+++ lib/kidsruby.rb
@@ -1,5 +1,6 @@
 $stdout.sync = true
 $stderr.sync = true
+require "rubygems"
 require File.expand_path(File.dirname(__FILE__) + "/kidsruby/interface")
 require File.expand_path(File.dirname(__FILE__) + "/kidsruby/dialogs")
 require File.expand_path(File.dirname(__FILE__) + "/kidsruby/turtle")
