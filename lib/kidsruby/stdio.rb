eb8d9eef7815a55f84571c47c4be319774c6f13f
Matt Scilipoti <matt@scilipoti.name>
2011-05-17 11:02:36 -0400

    StdIo: Move .puts up.  #dry

--- lib/kidsruby/stdio.rb
+++ lib/kidsruby/stdio.rb
@@ -2,6 +2,10 @@ class KidsRubyStdIo
   def initialize
     @iface = InterfaceHelper.new.get_interface
   end
+
+  def puts(data)
+    write(data.to_s + "\n")
+  end
 end
 
 class StdOut < KidsRubyStdIo
@@ -9,20 +13,12 @@ class StdOut < KidsRubyStdIo
     t = data.gsub(/\n/,"<br/>")
     @iface.call("append", t)
   end
-
-  def puts(data)
-    write(data.to_s + "\n")
-  end
 end
 
 class StdErr < KidsRubyStdIo
   def write(data)
     t = data.gsub(/\n/,"<br/>")
     @iface.call("appendError", t)
-  end
-
-  def puts(data)
-    write(data.to_s + "\n")
   end
 end
 
