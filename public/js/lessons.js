2fe73aefbc41370b21e0f88abc1a8d4f85482075
deadprogrammer <ron.evans@gmail.com>
2011-10-02 07:49:17 -0700

    Handle fact that webkit does not use same default value for display property

--- public/js/lessons.js
+++ public/js/lessons.js
@@ -2,24 +2,22 @@ jQuery(document).ready(function($){
 
   $('article.lesson:gt(0)').hide();
   $('#next-lesson').click(function(){
-    var $current = $('article.lesson:visible');
+    var $current =  $("article").filter(function() { return $(this).css("display") == "inline" || $(this).css("display") == "block" });
     if($current.next('article.lesson').length == 0)
       return false;
-      
-    $current.slideUp(500, function(){
-      $(this).next().slideDown(500);
-    });
+
+    $current.hide();
+    $current.next().show();
     return false;
   });
 
   $('#prev-lesson').click(function(){
-    var $current = $('article.lesson:visible');
+    var $current =  $("article").filter(function() { return $(this).css("display") == "inline" || $(this).css("display") == "block" });
     if($current.prev('article.lesson').length == 0)
       return false;
-  
-    $current.slideUp(500, function(){
-      $(this).prev().slideDown(500);
-    });
+
+    $current.hide();
+    $current.prev().show();
     return false;
   });
 });
