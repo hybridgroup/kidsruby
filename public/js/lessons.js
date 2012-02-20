739c9d75722cf920550eed29c4c24795ebc087a0
deadprogrammer <ron.evans@gmail.com>
2012-02-19 22:37:25 -0800

    Convert Ruby4Kids lesson 2 to new format

--- public/js/lessons.js
+++ public/js/lessons.js
@@ -1,5 +1,28 @@
-jQuery(document).ready(function($){
+function formatCode(t) {
+  $(t).snippet("ruby", {style:"pablo", showNum:true})
+}
 
+function loadCode(target, src){
+  $.ajax({
+    url: src,
+    type: "GET",
+    crossdomain: true,
+    dataType: 'text',
+    success: function(data) {
+      $(target).text(data);
+      formatCode(target);
+    },
+    error: function(x, s, t) {
+      // handles special case for windows webkit
+      if (x.status == 0) {
+        $(target).text(x.responseText);
+        formatCode(target);
+      }
+    }
+  });
+}
+
+jQuery(document).ready(function($){
   $('article.lesson:gt(0)').hide();
   $('#next-lesson').click(function(){
     var $current =  $("article").filter(function() { return $(this).css("display") == "inline" || $(this).css("display") == "block" });
