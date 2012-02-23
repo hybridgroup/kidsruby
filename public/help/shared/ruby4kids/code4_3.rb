aef4efd35d3a0c206e7506750d8b25b739052ebb
Omar Patel <omar@hybridgroup.com>
2012-02-22 21:32:18 -0800

    end of lesson complete codes

--- public/help/shared/ruby4kids/code4_3.rb
+++ public/help/shared/ruby4kids/code4_3.rb
@@ -8,6 +8,8 @@ class MyGame < Gosu::Window
   end
 
   def update
+    if button_down? Gosu::Button::KbLeft
+      @player1.move_left
   end
 
   def draw
@@ -19,12 +21,23 @@ class Player
   def initialize(game_window)
     @game_window = game_window
     @icon = Gosu::Image.new(@game_window, "gosu/player1.png", true)
+    @x = 50
+    @y = 50
   end
 
   def draw
-    @icon.draw(50,50,1)
+    @icon.draw(@x,@y,1)
+  end
+
+  def move_left
+    if @x < 0
+      @x = 0
+    else
+      @x = @x - 10
+    end
   end
 end
 
 window = MyGame.new
 window.show
+
