47fdffa319aafc28699c182538caa7d27fe58f45
deadprogrammer <ron.evans@gmail.com>
2012-02-24 17:03:05 -0800

    Editing lessons 6/7 so they have better flow

--- public/help/shared/ruby4kids/code7_5.rb
+++ public/help/shared/ruby4kids/code7_5.rb
@@ -5,4 +5,3 @@ end
 def y
   @y
 end
-
--- public/help/shared/ruby4kids/code7_6.rb
+++ public/help/shared/ruby4kids/code7_6.rb
@@ -1,6 +1,6 @@
 def initialize(game_window)
-    @game_window = game_window
-    @icon = Gosu::Image.new(@game_window, "images/ball.png", true)
-    @x = 50
-    @y = 150
-
+  @game_window = game_window
+  @icon = Gosu::Image.new(@game_window, "images/ball.png", true)
+  @x = 50
+  @y = 150
+end
