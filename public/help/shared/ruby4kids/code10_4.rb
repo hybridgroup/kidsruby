bc0fd47537b33a287d6907f32c260efd76b8d484
Omar Patel <omar@hybridgroup.com>
2012-02-26 19:50:56 -0800

    Added next and back links to bottom of sections and deleted lesson 10

--- public/help/shared/ruby4kids/code10_4.rb
+++
@@ -1,7 +0,0 @@
-def initialize
-  super(600, 400, false)
-  @player1 = Player1.new(self)
-  @ball = Ball.new(self)
-  @pause = false
-  @font = Gosu::Font.new(self, 'System', 24)
-end
--- public/help/shared/ruby4kids/code10_5.rb
+++
@@ -1,5 +0,0 @@
-def draw
-  @player1.draw
-  @ball.draw
-  @font.draw("The game is paused.", 50, 200, 10) if @pause == true
-end
