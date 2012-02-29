79a3ae29bc00e746c6bdb5c17753acbc4752c780
deadprogrammer <ron.evans@gmail.com>
2012-02-29 15:46:52 -0800

    Fix syntax errors

--- public/help/shared/ruby4kids/code7_7.rb
+++ public/help/shared/ruby4kids/code7_7.rb
@@ -96,7 +96,7 @@ end
 class Ball
   def initialize(game_window)
     @game_window = game_window
-    @icon = Gosu::Image.new(@game_window, "Images/ball.png", true)
+    @icon = Gosu::Image.new(@game_window, "gosu/asteroid.png", true)
     @x = rand(@game_window.width)
     @y = 0 
   end
