c56e5cddbe5faa269feb684d141f749ef164a74f
Rafael Magana <raf.magana@gmail.com>
2012-06-26 11:26:07 -0500

    delete index_es, add js/i18n.es.js

---
+++ public/js/i18n/es.js
@@ -0,0 +1,14 @@
+{
+  "buttons": {
+    "run": "Ejecutar",
+    "clear": "Borrar",
+    "open": "Abrir",
+    "save": "Guardar"
+  },
+  "tabs": {
+    "help-link": "Ayuda",
+    "output-link": "Resultado",
+    "turtle-link": "Tortuga"
+  }
+}
+
