6e6c30debb4628a855287ef241df2c17bec3f324
Rafael Magana <raf.magana@gmail.com>
2012-06-27 16:11:21 -0500

    [es] add translations for new js/i18n keys

--- public/js/i18n/es.js
+++ public/js/i18n/es.js
@@ -3,12 +3,16 @@
     "run": "Ejecutar",
     "clear": "Borrar",
     "open": "Abrir",
-    "save": "Guardar"
+    "save": "Guardar",
+    "stop": "Detener"
   },
   "tabs": {
     "help-link": "Ayuda",
     "output-link": "Resultado",
     "turtle-link": "Tortuga"
+  },
+  "editor": {
+    "your-code": "Escribe tu código justo debajo de esta línea",
+    "confirm-clear": "Estás seguro de querer borrar tu código Ruby del editor?"
   }
 }
-
